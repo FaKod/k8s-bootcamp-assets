4e49ecdb669a4b7c419d5bc398fb116cb2f66519
Simon Jost <simon.jost@innoq.com>
2017-10-12 13:51:48 +0200

    adds form web component

---
+++ js/k8s-form.js
@@ -0,0 +1,82 @@
+(function () {
+  const parser = new DOMParser()
+
+  function serialize(formData) {
+    return [...formData.entries()]
+      .map(e => `${encodeURIComponent(e[0])}=${encodeURIComponent(e[1])}`)
+      .join('&')
+  }
+
+  function splitSelectors(selctorStr) {
+    return selctorStr.split(',').map(t => t.trim());
+  }
+
+  function replaceTargets(current, remote, selectors) {
+    console.log(selectors)
+    selectors.forEach(selector => {
+      let source = current.querySelector(selector);
+      if (source) {
+        let target = remote.querySelector(selector);
+        source.parentNode.replaceChild(target, source);
+      }
+    });
+  }
+
+  async function fetchDocument($form) {
+    let options = {
+      method: $form.method.toUpperCase()
+    }
+
+    console.log(options.method)
+    switch (options.method) {
+      case 'POST': 
+        uri = $form.action || location.pathname
+        options.body = new FormData($form)
+        break;
+      default:
+        uri = `${$form.action}?${serialize(new FormData($form))}`
+    }
+
+    try {
+      const rs = await fetch(uri, options);
+      const htmlString = await rs.text();
+      return parser.parseFromString(htmlString, 'text/html')
+    } catch (e) {
+      e.message = `Failed to fetch document from ${uri}`
+      throw e
+    }
+  }
+
+  function createOnSubmit($form, selectors) {
+    return async event => {
+      event.preventDefault()
+      const $doc = await fetchDocument($form)
+      replaceTargets(document, $doc, selectors)
+    }
+  }
+
+  class K8sForm extends HTMLElement {
+    static get is() {
+      return 'k8s-form'
+    }
+
+    static get observedAttributes() {
+      return [
+        'data-target'
+      ]
+    }
+
+    attributeChangedCallback(attr, _, value) {
+      this[attr] = value;
+    }
+
+    connectedCallback() {
+      let targets = splitSelectors(this['data-target'])
+      let $form = this.querySelector('form')
+
+      $form.addEventListener('submit', createOnSubmit($form, targets))
+    }
+  }
+
+  window.customElements.define(K8sForm.is, K8sForm)
+}());
